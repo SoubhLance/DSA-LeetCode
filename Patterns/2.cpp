131e2595e4fd97c618f34ced530cf8d3c21e4353
soubhlance <soubhiksadhu1981@gmail.com>
2026-01-11 23:39:39 +0530

    patterns

--- Patterns/2.cpp
+++ Patterns/2.cpp
@@ -1,6 +1,6 @@
-// ****
-// ****
-// ****
+// *
+// **
+// ***
 // ****
 // WAP to print the pattern 
 
@@ -16,7 +16,7 @@ int main()
     cin>>n;
     for( int i =0; i<n;i++)
     {
-        for(int j = 0; j<n;j++)\
+        for(int j = 0; j<=i;j++)\
         {
             cout<<("*");
         }
