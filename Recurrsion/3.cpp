b7fac45d15205400f8dd53e76cd7fef7ee953a2c
soubhlance <soubhiksadhu1981@gmail.com>
2026-01-19 04:39:10 +0530

    update_recurrsion

---
+++ Recurrsion/3.cpp
@@ -0,0 +1,19 @@
+// print from N to 1
+
+#include<iostream>
+using namespace std;
+
+void print(int n)
+{   
+    if (n == 0) return;   
+    cout << n << endl;
+    print(n-1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    print(n);
+    return 0;
+}
