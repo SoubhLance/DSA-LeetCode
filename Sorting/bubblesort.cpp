a04c035449a79b0a4417ccfd47599b9a8cd528e8
Soubhlance <soubhiksadhu1981@gmail.com>
2026-02-17 22:56:52 +0530

    bubble sort

---
+++ Sorting/bubblesort.cpp
@@ -0,0 +1,40 @@
+//Bubble Sorting
+// Here in the bubble sort we push the maximium element to the end of the array in each iteration by adjacent swaps 
+
+#include<iostream>
+using namespace std;
+
+int swap(int &a, int &b)
+{
+    int temp = a;
+    a=b;
+    b=temp;
+}
+
+int bubble_sort(int arr[],int n)
+{
+    for(int i=n-1;i>=1;i--)
+    {
+        for(int j=0;j<=i-1;j++)
+        {
+            if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin>> n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    bubble_sort(arr,n);
+    for(int i=0;i<n;i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
