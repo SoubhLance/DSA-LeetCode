ea585698fe98af03c5a26f7bf4b167c344785991
Soubhlance <soubhiksadhu1981@gmail.com>
2026-02-20 22:51:14 +0530

    fix

---
+++ Sorting/mergesort.cpp
@@ -0,0 +1,9 @@
+//Merge Sort in C++
+// Merge Sort is a divide and conquer algorithm that divides the input array into two halves, calls itself for the two halves, and then merges the two sorted halves.
+// The time complexity of merge sort is O(n log n) in all cases.
+// The space complexity of merge sort is O(n) because we need to create temporary arrays to hold the two halves of the array during the merge process.
+
+
+#include <iostream>
+using namespace std;
+
