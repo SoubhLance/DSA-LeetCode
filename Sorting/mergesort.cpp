475a1eda89daf8ed41e4aab6cf42bff3ecc42520
Soubhlance <soubhiksadhu1981@gmail.com>
2026-04-19 13:29:30 +0530

    merge Sort

--- Sorting/mergesort.cpp
+++ Sorting/mergesort.cpp
@@ -5,8 +5,81 @@
 
 
 #include <iostream>
+#include <vector>
+#include<algorithm>
 using namespace std;
 
-void Merge_Sort(){
 
+void merge(vector<int> &arr, int low, int mid, int high){
+    vector<int> temp;
+    //low to mid
+    //mid+1 to high
+    int left = low;
+    int right = mid+1;
+    
+    while(left<=mid && right<=high){
+        if(arr[left]<=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while(left <= mid) 
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right <= high) 
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for(int i = low; i <= high; i++) 
+    {
+        arr[i] = temp[i - low];
+    }   
+}
+
+void MS(vector<int> &arr, int low, int high){
+    if(low==high) return;
+    int mid = (low+high)/2;
+    //left half
+    MS(arr,low,mid);
+    //right half
+    MS(arr,mid+1,high);
+    //merge
+    merge(arr,low,mid,high);
+}
+
+void Merge_Sort(vector<int> &arr, int n ){
+    MS(arr,0,n-1);
+}
+
+
+
+int main(){
+    vector<int> arr;
+    int n;
+    cout<<"Enter the number of elements in the array: "<<endl;
+    cin>>n;
+    for(int i = 0; i < n; i++){
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+    cout<<"The elements of the array are: ";
+    for(auto x: arr){
+        cout<<x<<" "<<endl;
+    }
+
+    Merge_Sort(arr,n);
+
+    cout<< "The sorted array is: "<<endl;
+    for(auto x: arr){           
+        cout<<x<<" ";
+    }
+    return 0;
 }
